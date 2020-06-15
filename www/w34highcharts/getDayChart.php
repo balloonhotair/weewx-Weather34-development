003efdf79f838ee9e0b0e8bdeb77409b9ffd2542
balloonhotair <51388216+balloonhotair@users.noreply.github.com>
2020-06-15 10:16:22 -0700

    Weewx IP in realtime.txt for day plots

--- www/w34highcharts/getDayChart.php
+++ www/w34highcharts/getDayChart.php
@@ -1,13 +1,22 @@
 <?php 
+    include('../w34CombinedData.php');
     $plot_info = explode(",",$_GET['plot_info']);
     $filenames = explode(":", $plot_info[1]);
+    putenv("PYTHONPATH=".$_GET['weewxpathbin']);
+    if (isset($weather['weewx_ip'])) 
+      $weewxserver_address = $weather['weewx_ip'];
+    if (!isset($weewxserver_port)) 
+      $weewxserver_port = 25252;
     for ($i = 0; $i < sizeof($filenames); $i++){ 
-      if (isset($weexserver_address) and isset($weexserver_port)){
-        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        socket_connect($socket, $weexserver_address, $weexserver_port);
+      if (isset($weewxserver_address) and isset($weewxserver_port)){
+        try {
+          $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+          socket_connect($socket, $weewxserver_address, $weewxserver_port);
+        }catch(Exception $e){
+          $weewxserver_port = NULL;
+          $weewxserver_address = NULL;
+        }
       }
-      else
-        putenv("PYTHONPATH=".$_GET['weewxpathbin']);
       $filename = $plot_info[0].$filenames[$i];
       if (file_exists($filename))
         unlink($filename);
@@ -17,34 +26,46 @@
         $s_file1 = explode(".", $filename)[0]."1.json";
         if (file_exists($s_file1))
           unlink($s_file1);
-        if (isset($weexserver_address) and isset($weexserver_port)) {
-          socket_write($socket, $cmd, strlen($cmd));
-          @socket_read($socket, 1, PHP_NORMAL_READ);
-          socket_close($socket);
+        if (isset($weewxserver_address) and isset($weewxserver_port)) {
+          try {
+            socket_write($socket, $cmd, strlen($cmd));
+            @socket_read($socket, 1, PHP_NORMAL_READ);
+            @socket_close($socket);
+          }catch(Exception $e){
+            shell_exec(escapeshellcmd($cmd));
+          }
         }
         else
-          $output = shell_exec(escapeshellcmd($cmd));
+          shell_exec(escapeshellcmd($cmd));
         rename($filename, $s_file1);
         $cmd = $plot_info[2]." ".$_GET['epoch1']." ".$filename.".tmpl ".getcwd();
         #print($cmd);
-        if (isset($weexserver_address) and isset($weexserver_port)) {
-          $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-          socket_connect($socket, $weexserver_address, $weexserver_port);
-          socket_write($socket, $cmd, strlen($cmd));
-          @socket_read($socket, 1, PHP_NORMAL_READ);
-          socket_close($socket);
+        if (isset($weewxserver_address) and isset($weewxserver_port)) {
+          try {
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            socket_connect($socket, $weewxserver_address, $weewxserver_port);
+            socket_write($socket, $cmd, strlen($cmd));
+            @socket_read($socket, 1, PHP_NORMAL_READ);
+            @socket_close($socket);
+          }catch(Exception $e){
+            shell_exec(escapeshellcmd($cmd));
+          }
         }
         else
-          $output = shell_exec(escapeshellcmd($cmd));
+          shell_exec(escapeshellcmd($cmd));
       }
       else {
-        if (isset($weexserver_address) and isset($weexserver_port)){
-          socket_write($socket, $cmd, strlen($cmd));
-          @socket_read($socket, 1, PHP_NORMAL_READ);
-          socket_close($socket);
+        if (isset($weewxserver_address) and isset($weewxserver_port)){
+          try {
+            socket_write($socket, $cmd, strlen($cmd));
+            @socket_read($socket, 1, PHP_NORMAL_READ);
+            @socket_close($socket);
+          }catch(Exception $e){
+            shell_exec(escapeshellcmd($cmd));
+          }
         }
         else
-          $output = shell_exec(escapeshellcmd($cmd));
+          shell_exec(escapeshellcmd($cmd));
       }
     }
 ?> 
